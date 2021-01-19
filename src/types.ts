7e446cae3eb07921f540a6bd9f265c1162b48094
João L. S. Silva <joao.lucas.ss@hotmail.com>
2021-01-19 14:59:39 -0300

    missing file

---
+++ src/types.ts
@@ -0,0 +1,78 @@
+export {}
+
+declare global {
+	export namespace Random {
+		/**
+		 * Possible types of module
+		 */
+		export type Type = "string" | "array" | "object" | "int" | "decimal" | "null" | "undefined"
+
+		/**
+		 * Integer configuration object
+		 */
+		export interface IntegerConfig {
+			min?: number
+			max?: number
+		}
+
+		/**
+		 * Decimal configuration object
+		 */
+		export interface DecimalConfig extends IntegerConfig {
+			decimalPlaces?: number
+		}
+
+		/**
+		 * Type configuration object
+		 */
+		export interface TypeConfig {
+			exclude?: Type[]
+		}
+
+		/**
+		 * Char configuration object
+		 */
+		export interface CharConfig {
+			charset?: string
+		}
+
+		/**
+		 * Word configuration object
+		 */
+		export interface WordConfig extends CharConfig {
+			length?: number
+		}
+
+		/**
+		 * Phrase configuration object
+		 */
+		export interface PhraseConfig extends CharConfig {
+			numberOfWords?: number
+			lengths?: number[]
+		}
+
+		export interface NonPrimitveConfig {
+			depth?: number
+			exclude?: Type[]
+			stringConfig?: WordConfig
+			integerConfig?: IntegerConfig
+			decimalConfig?: DecimalConfig
+			arrayConfig?: ArrayConfig
+			objectConfig?: ObjectConfig
+		}
+
+		/**
+		 * Object configuration object
+		 */
+		export interface ObjectConfig extends NonPrimitveConfig {
+			numberOfProps?: number
+		}
+
+		/**
+		 * Array configuration object
+		 */
+		export interface ArrayConfig extends NonPrimitveConfig {
+			length?: number
+		}
+	}
+}
